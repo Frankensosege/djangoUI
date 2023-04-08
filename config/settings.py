989c3fb028342ae3594fdd0aef5fe757739e7ff2
Frankensosege <frnakesosege@gmail.com>
2023-04-08 16:47:43 +0900

    20230408-3

--- config/settings.py
+++ config/settings.py
@@ -130,11 +130,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
 # LANGUAGE_CODE = 'en-us'
-LANGUAGE_CODE = 'ko'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+# TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
+USE_L10N = True
 
 USE_TZ = True
 
